73445e32e1fbecd9daa86913f2f1518315725ae4
Yusuke Sakurai <kerokerokerop@gmail.com>
2022-10-26 17:27:45 +0900

    bump: node16 (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import * as core from "@actions/core"
-import * as io from "@actions/io"
-import {installLake} from "./install";
+import * as core from "@actions/core";
+import * as io from "@actions/io";
+import { installLake } from "./install";
 
-async  function main() {
-    const version = core.getInput("lake-version");
-    try {
-        if(!(await io.which("lake", false))) {
-            await installLake(version)
-        }
-    } catch (e) {
-        core.setFailed(e.message)
+async function main() {
+  const version = core.getInput("lake-version");
+  try {
+    if (!(await io.which("lake", false))) {
+      await installLake(version);
     }
+  } catch (e) {
+    core.setFailed(e.message);
+  }
 }
 
 if (require.main === module) {
-    main();
+  main();
 }
